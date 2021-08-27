8d1e51702cc3b80675a94c702deb4b1e1deb3931
Jobayer Hossain <jobayerhossain977@gmail.com>
2021-08-27 14:21:18 +0600

    master: Added

--- admin-panel/src/@modules/auth/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./hooks/useAuth"
-export * from "./interfaces"
-export * from "./lib"
-export * from "./routes"
--- admin-panel/src/@shared/config/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./axios/core-axios-instantance"
-export * from "./react-query/react-query"
-export * from "./redux/rootReducer"
-export * from "./redux/store"
